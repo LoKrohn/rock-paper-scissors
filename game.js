f690463bd8b446a44cfe542166e29040969ec79c
L. Krohn <LNKrohn@gmail.com>
2022-05-17 17:09:15 -0700

    add gameplay logic for single round

--- game.js
+++ game.js
@@ -17,6 +17,27 @@ function computerPlay() {
     
 }
 
+function singleRound () {
+    if (playerDecision === computerDecision) {
+         return "There has been a tie!";
+    }
+    else if ((playerDecision == "rock" && computerDecision == "paper") ||
+            (playerDecision == "paper" && computerDecision == "scissors") ||
+            (playerDecision == "scissors" && computerDecision == "rock")) {
+                return "You have lost! How very unfortunate."
+            }
+    else if ((playerDecision == "paper" && computerDecision == "rock") ||
+            (playerDecision == "scissors" && computerDecision == "paper") ||
+            (playerDecision == "rock" && computerDecision == "scissors")) {
+                return "You have won! It is an auspicious day."
+            }
+    else {
+        return "we haven't made this part yet"
+    }
+}
 
-console.log(playerDecision)
+
+console.log(playerDecision);
 console.log(computerDecision);
+console.log(singleRound());
+
