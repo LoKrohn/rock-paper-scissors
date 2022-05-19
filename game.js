ace840e460655e79fc55ac001fe2ac229d4f6567
L. Krohn <LNKrohn@gmail.com>
2022-05-19 10:20:30 -0700

    add 5-round limit to game

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
-let playerDecision = prompt("Rock, Paper, or Scissors?").toLowerCase();
-let computerDecision = computerPlay();
-
+let round = 0;
+let userScore = 0;
+let computerScore = 0;
+let playerDecision;
+let computerDecision;
 
 function computerPlay() {
     switch (randomNumber = Math.floor(Math.random()*3)){
@@ -17,18 +19,26 @@ function computerPlay() {
     
 }
 
-function singleRound () {
+function playRound () {
+    playerDecision = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    computerDecision = computerPlay();
     if (playerDecision === computerDecision) {
+         round++;
          return "There has been a tie!";
     }
     else if ((playerDecision == "rock" && computerDecision == "paper") ||
             (playerDecision == "paper" && computerDecision == "scissors") ||
             (playerDecision == "scissors" && computerDecision == "rock")) {
+                round++;
+                computerScore++;
                 return "You have lost! How very unfortunate."
+
             }
     else if ((playerDecision == "paper" && computerDecision == "rock") ||
             (playerDecision == "scissors" && computerDecision == "paper") ||
             (playerDecision == "rock" && computerDecision == "scissors")) {
+                round++;
+                userScore++;
                 return "You have won! It is an auspicious day."
             }
     else {
@@ -36,8 +46,16 @@ function singleRound () {
     }
 }
 
+function playGame() {
+    while (round<5) {
+        console.log(playRound());
+        console.log(playerDecision);
+        console.log(computerDecision);
+    }
+    alert("The game is over! Your score:" + userScore + " Computer score: " + computerScore)
+}
 
-console.log(playerDecision);
-console.log(computerDecision);
-console.log(singleRound());
+playGame();
+
+
 
